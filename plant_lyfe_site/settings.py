7d1ea5cda179deeb0bcc5173e466415d0edf431a
Blithe Rocher <blithe@bignerdranch.com>
2014-09-04 14:09:39 -0700

    Setup database.

--- plant_lyfe_site/settings.py
+++ plant_lyfe_site/settings.py
@@ -57,8 +57,11 @@ WSGI_APPLICATION = 'plant_lyfe_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'plant_lyfe_dev',                      
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
+        'HOST': ''
     }
 }
 
