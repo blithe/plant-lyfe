b93e2e9e0e61d1169c9845ad45deb508f2508084
Blithe Rocher <blithe@bignerdranch.com>
2014-09-04 16:26:15 -0700

    Add Plant model.

--- plant_lyfe_app/models.py
+++ plant_lyfe_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Plant(models.Model):
+    common_name = models.CharField(max_length=200)
+    subclass = models.CharField(max_length=200)
+    order = models.CharField(max_length=200)
+    family = models.CharField(max_length=200)
+    genus = models.CharField(max_length=200)
+    species = models.CharField(max_length=200)
--- plant_lyfe_app/tests.py
+++ plant_lyfe_app/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from plant_lyfe_app.models import Plant
 
-# Create your tests here.
+class PlantTestCase(TestCase):
+    def setUp(self):
+        Plant.objects.create(common_name="bigleaf maple",
+                             subclass="Rosidae",
+                             order="Sapindales",
+                             family="Aceraceae",
+                             genus="Acer L.",
+                             species="Acer macrophyllum Pursh",
+                             )
+
+    def test_plants_have_subclasses(self):
+        """Plants have all the correct attributes"""
+        maple = Plant.objects.get(common_name="bigleaf maple")
+        self.assertEqual(maple.subclass, 'Rosidae')
+        self.assertEqual(maple.order, 'Sapindales')
+        self.assertEqual(maple.family, 'Aceraceae')
+        self.assertEqual(maple.genus, 'Acer L.')
+        self.assertEqual(maple.species, 'Acer macrophyllum Pursh')
