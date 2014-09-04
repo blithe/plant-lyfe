a9c36efb70ab1088eb24c99f806134fe83674899
Blithe Rocher <blithe@bignerdranch.com>
2014-09-04 15:48:46 -0700

    Add index view.

--- plant_lyfe_site/urls.py
+++ plant_lyfe_site/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+import plant_lyfe_app.views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'plant_lyfe_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', plant_lyfe_app.views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
