c238703710e024ca06e1716e7d7ab7d1430d660f
Blithe Rocher <blithe@bignerdranch.com>
2014-09-04 14:15:57 -0700

    Additional setup. * Updated settings * Created app * Added South * Enabled Django Admin

--- plant_lyfe_site/settings.py
+++ plant_lyfe_site/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'plant_lyfe_app',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
