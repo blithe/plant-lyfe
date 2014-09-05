e1a080120e7feedd13b25f15f736793f74c7d26c
Blithe Rocher <blithe@bignerdranch.com>
2014-09-05 08:35:10 -0700

    Add Leaves model and nested routes.

--- plant_lyfe_app/models.py
+++ plant_lyfe_app/models.py
@@ -7,3 +7,12 @@ class Plant(models.Model):
     family = models.CharField(max_length=200)
     genus = models.CharField(max_length=200)
     species = models.CharField(max_length=200)
+
+class Leaf(models.Model):
+    plant = models.ForeignKey(Plant, related_name='leaves')
+    placement = models.CharField(max_length=200)
+    blade = models.CharField(max_length=200)
+    veins = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    date_found = models.DateField('date-found')
+    url = "/dicots/%(plant.common_name)-%(plant-id)/leaf/%(id)"
--- plant_lyfe_site/urls.py
+++ plant_lyfe_site/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 from plant_lyfe_app.models import Plant
+from plant_lyfe_app.models import Leaf
 
-from rest_framework import serializers, viewsets, routers
+from rest_framework import serializers, viewsets
+from rest_framework_nested import routers
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,19 +13,37 @@ import plant_lyfe_app.views
 
 # Serializers define the API representation.
 class PlantSerializer(serializers.HyperlinkedModelSerializer):
+    leaves = serializers.SlugRelatedField(many=True, slug_field='url')
+
     class Meta:
         model = Plant
-        fields = ('id', 'common_name', 'subclass', 'order', 'family', 'genus', 'species')
+        fields = ('id', 'common_name', 'subclass', 'order', 'family', 'genus', 'species', 'leaves')
+
+    def plant_name(self, obj):
+      return "plant-%(name)s" % {"name": obj.id}
+
+class LeafSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Leaf
+        fields = ('id', 'plant', 'placement', 'date_found')
 
 # ViewSets define the view behavior.
 class PlantViewSet(viewsets.ModelViewSet):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
 
+class LeafViewSet(viewsets.ModelViewSet):
+    queryset = Leaf.objects.all()
+    serializer_class = LeafSerializer
+
 
 # Routers provide a way of automatically determining the URL conf.
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r'dicots', PlantViewSet)
+
+dicots_router = routers.NestedSimpleRouter(router, r'dicots', lookup='dicots')
+dicots_router.register(r'leaf', LeafViewSet)
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -31,6 +51,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^', include(router.urls)),
+    url(r'^', include(dicots_router.urls)),
     url(r'^index', plant_lyfe_app.views.index, name='index'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
