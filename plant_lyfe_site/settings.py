6a1483ecada1fc9f000ce9f8a5ed5ce958a788fb
Blithe Rocher <blithe@bignerdranch.com>
2014-09-05 08:36:37 -0700

    Remove authentication requirement.

--- plant_lyfe_site/settings.py
+++ plant_lyfe_site/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'plant_lyfe_app',
     'rest_framework',
+    'django_extensions',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -59,7 +60,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.AllowAny'
     ]
 }
 
