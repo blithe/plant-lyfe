2b78ea09117ff8a7928e2937f9cdcf5696d4b0d8
Blithe Rocher <blithe@bignerdranch.com>
2014-09-06 23:42:43 -0700

    Rework Plant and Leaf views completely and add tests. * Remove ViewSets and create custom views. * Update routes to match new actions.
    Still to do:
    * PUT to dicot name creates a Leaf
    * searching / filtering

--- plant_lyfe_app/tests.py
+++ plant_lyfe_app/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
-from plant_lyfe_app.models import Plant
+from plant_lyfe_app.models import Plant, Leaf
+from rest_framework import status
+from rest_framework.test import APITestCase
+import json
+import code
 
 class PlantTestCase(TestCase):
     def setUp(self):
@@ -19,3 +23,263 @@ class PlantTestCase(TestCase):
         self.assertEqual(maple.family, 'Aceraceae')
         self.assertEqual(maple.genus, 'Acer L.')
         self.assertEqual(maple.species, 'Acer macrophyllum Pursh')
+
+class ApiTestCase(APITestCase):
+    def test_get_dicots_returns_plant_list(self):
+      """URL: /dicots GET - Should return a list of all Plant resources."""
+      self.maxDiff=None
+      plant1 = Plant.objects.create(common_name="bigleaf maple",
+                             subclass="Rosidae",
+                             order="Sapindales",
+                             family="Aceraceae",
+                             genus="Acer L.",
+                             species="Acer macrophyllum Pursh",
+                             )
+      plant2 = Plant.objects.create(common_name="big green thing",
+                           subclass="Rosidae",
+                           species="latinus nameus",
+                           )
+      leaf1 = Leaf.objects.create(plant=plant1, date_found="2014-01-01")
+      leaf2 = Leaf.objects.create(plant=plant1, date_found="2014-01-01")
+      leaf3 = Leaf.objects.create(plant=plant2, date_found="2014-01-01")
+      leaf4 = Leaf.objects.create(plant=plant2, date_found="2014-01-01")
+      leaf5 = Leaf.objects.create(plant=plant2, date_found="2014-01-01")
+
+      response = self.client.get('/dicots/')
+      expected_data = {
+          "plants": [
+            {
+              "id": "plant-%i" % (plant1.id),
+              "common_name": plant1.common_name,
+              "species": plant1.species,
+              "leaves": [
+                "/dicots/%s/leaf/%i" % (plant1.slug, leaf2.id),
+                "/dicots/%s/leaf/%i" % (plant1.slug, leaf1.id)
+              ]
+            },
+            {
+              "id": "plant-%i" % (plant2.id),
+              "common_name": plant2.common_name,
+              "species": plant2.species,
+              "leaves": [
+                "/dicots/%s/leaf/%i" % (plant2.slug, leaf5.id),
+                "/dicots/%s/leaf/%i" % (plant2.slug, leaf4.id),
+                "/dicots/%s/leaf/%i" % (plant2.slug, leaf3.id)
+              ]
+            }
+          ]
+        }
+      self.assertJSONEqual(response.content, json.dumps(expected_data))
+
+    def test_get_dicot_name_returns_plant_details(self):
+      """URL: /dicots/bigleaf-maple GET-Should return a representation of the Plant, including a list of linked Leaf resources"""
+      plant = Plant.objects.create(common_name="bigleaf maple",
+                             subclass="Rosidae",
+                             order="Sapindales",
+                             family="Aceraceae",
+                             genus="Acer L.",
+                             species="Acer macrophyllum Pursh",
+                             )
+      leaf1 = Leaf.objects.create(plant=plant, date_found="2014-01-01")
+      leaf2 = Leaf.objects.create(plant=plant, date_found="2014-01-01")
+      expected_data = {
+          "id": "plant-%i" % (plant.id),
+          "common_name": plant.common_name,
+          "subclass": plant.subclass,
+          "order": plant.order,
+          "family": plant.family,
+          "genus": plant.genus,
+          "species": plant.species,
+          "leaves": [
+            "/dicots/%s/leaf/%i" % (plant.slug, leaf2.id),
+            "/dicots/%s/leaf/%i" % (plant.slug, leaf1.id)
+          ]
+        }
+
+      response = self.client.get('/dicots/bigleaf-maple/')
+      self.assertJSONEqual(response.content, json.dumps(expected_data))
+
+    def test_put_dicot_name_creates_plant(self):
+      """PUT /dicots/mahogany - Should create a Plant resource"""
+      request_data = {
+                "common_name": "mahogany",
+                "subclass": "Rosidae",
+                "order": "Sapindales",
+                "family": "Meliaceae",
+                "genus": "Swietenia",
+                "species": "Sweitenia mahagoni"
+              }
+      response = self.client.put('/dicots/mahogany/', request_data, format='json')
+      created_plant = Plant.objects.latest('id')
+      expected_data = {
+          "id": "plant-%i" % (created_plant.id),
+          "common_name": created_plant.common_name,
+          "subclass": created_plant.subclass,
+          "order": created_plant.order,
+          "family": created_plant.family,
+          "genus": created_plant.genus,
+          "species": created_plant.species,
+          "leaves": []
+        }
+      self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+      self.assertJSONEqual(response.content, json.dumps(expected_data))
+
+
+    def test_delete_dicot_name_deletes_plant(self):
+      """DELETE /dicots/mahogany - Should delete a Plant resource"""
+      Plant.objects.create(common_name="mahogany plant",
+                           species="Acer macrophyllum Pursh",
+                           )
+      response = self.client.delete('/dicots/mahogany-plant/')
+      self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_post_dicot_name_updates_plant(self):
+      """POST /dicots/mahogany - Should update a Plant resource"""
+      plant = Plant.objects.create(common_name="mahogany",
+                                   subclass="Rosidae",
+                                   order="Sapindales",
+                                   family="Aceraceae",
+                                   genus="Acer L.",
+                                   species="Acer macrophyllum Pursh",
+                                   )
+      request_data = {
+                "common_name": "mahogany",
+                "subclass": "I dunno",
+                "order": "something else",
+                "family": "Whee!",
+                "genus": "Swietenia",
+                "species": "Sweitenia mahagoni"
+              }
+      expected_data = {
+          "id": "plant-%i" % (plant.id),
+          "common_name": "mahogany",
+          "subclass": "I dunno",
+          "order": "something else",
+          "family": "Whee!",
+          "genus": "Swietenia",
+          "species": "Sweitenia mahagoni",
+          "leaves": []
+        }
+
+      response = self.client.post('/dicots/mahogany/', request_data, format='json')
+      self.assertEqual(response.status_code, status.HTTP_200_OK)
+      self.assertJSONEqual(response.content, json.dumps(expected_data))
+
+
+    def test_post_dicot_name_creates_leaf(self):
+      """POST /dicots/mahogany - Should Create a Leaf resource subordinate to the Plant resource"""
+      plant = Plant.objects.create(common_name="mahogany",
+                                   subclass="Rosidae",
+                                   order="Sapindales",
+                                   family="Aceraceae",
+                                   genus="Acer L.",
+                                   species="Acer macrophyllum Pursh",
+                                   )
+      request_data = {
+                "placement": "opposite",
+                "blade": "palmately compound",
+                "veins": "penniveined",
+                "location": "Vancouver, BC",
+                "date-found": "2014-01-01"
+              }
+
+      response = self.client.post('/dicots/mahogany/', request_data, format='json')
+      created_leaf = Leaf.objects.latest('id')
+      expected_data = {
+          "id": "leaf-%i" % (created_leaf.id),
+          "plant": "plant-%i" % (plant.id),
+          "placement": "opposite",
+          "blade": "palmately compound",
+          "veins": "penniveined",
+          "location": "Vancouver, BC",
+          "date-found": "2014-01-01"
+        }
+
+      self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+      self.assertJSONEqual(response.content, json.dumps(expected_data))
+
+    def test_get_leaf_name_returns_leaf_details(self):
+      """URL: /dicots/bigleaf-maple/leaf/110 GET-Should return a representation of the Leaf"""
+      plant = Plant.objects.create(common_name="bigleaf maple",
+                             subclass="Rosidae",
+                             order="Sapindales",
+                             family="Aceraceae",
+                             genus="Acer L.",
+                             species="Acer macrophyllum Pursh",
+                             )
+      leaf = Leaf.objects.create(plant=plant,
+                                 placement="opposite",
+                                 blade="palmately compound",
+                                 veins="penniveined",
+                                 location="Vancouver, BC",
+                                 date_found="2014-01-01"
+                                )
+      expected_data = {
+          "id": "leaf-%i" % (leaf.id),
+          "plant": "plant-%i" % (plant.id),
+          "placement": "opposite",
+          "blade": "palmately compound",
+          "veins": "penniveined",
+          "location": "Vancouver, BC",
+          "date_found": "2014-01-01"
+        }
+
+      response = self.client.get("/dicots/bigleaf-maple/leaf/%i/" % (leaf.id))
+      self.assertJSONEqual(response.content, json.dumps(expected_data))
+
+    def test_delete_leaf_name_deletes_leaf(self):
+      """URL: /dicots/bigleaf-maple/leaf/110 DELETE - Should delete the Leaf resource"""
+      plant = Plant.objects.create(common_name="bigleaf maple",
+                             subclass="Rosidae",
+                             order="Sapindales",
+                             family="Aceraceae",
+                             genus="Acer L.",
+                             species="Acer macrophyllum Pursh",
+                             )
+      leaf = Leaf.objects.create(plant=plant,
+                                 placement="opposite",
+                                 blade="palmately compound",
+                                 veins="penniveined",
+                                 location="Vancouver, BC",
+                                 date_found="2014-01-01"
+                                )
+
+      response = self.client.delete("/dicots/bigleaf-maple/leaf/%i/" % (leaf.id))
+      self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_post_leaf_updates_leaf(self):
+      """POST /dicots/mahogany/leaf/110 - POST - Should update the Leaf resource"""
+      plant = Plant.objects.create(common_name="mahogany",
+                             subclass="Rosidae",
+                             order="Sapindales",
+                             family="Aceraceae",
+                             genus="Acer L.",
+                             species="Acer macrophyllum Pursh",
+                             )
+      leaf = Leaf.objects.create(plant=plant,
+                                 placement="opposite",
+                                 blade="palmately compound",
+                                 veins="penniveined",
+                                 location="Vancouver, BC",
+                                 date_found="2014-01-01"
+                                )
+      request_data = {
+                "placement": "other side",
+                "blade": "not sure",
+                "veins": "ALL the veins",
+                "location": "Oakland, CA",
+                "date_found": "2010-10-10"
+              }
+      expected_data = {
+          "id": "leaf-%i" % (leaf.id),
+          "plant": "plant-%i" % (plant.id),
+          "placement": "other side",
+          "blade": "not sure",
+          "veins": "ALL the veins",
+          "location": "Oakland, CA",
+          "date_found": "2010-10-10"
+        }
+
+      response = self.client.post("/dicots/mahogany/leaf/%i/" % (leaf.id), request_data, format='json')
+      self.assertEqual(response.status_code, status.HTTP_200_OK)
+      self.assertJSONEqual(response.content, json.dumps(expected_data))
--- plant_lyfe_site/urls.py
+++ plant_lyfe_site/urls.py
@@ -1,15 +1,30 @@
 from django.conf.urls import patterns, include, url
 
-from plant_lyfe_app.models import Plant
-from plant_lyfe_app.models import Leaf
+from plant_lyfe_app.models import Plant, Leaf
 
-from rest_framework import serializers, viewsets
-from rest_framework_nested import routers
+from django.http import Http404
+
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+import code
 
 from django.contrib import admin
 admin.autodiscover()
 
 # Serializers define the API representation.
+class PlantListSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.SerializerMethodField('label_id')
+    leaves = serializers.RelatedField(many=True)
+
+    class Meta:
+        model = Plant
+        fields = ('id', 'common_name', 'species', 'leaves')
+
+    def label_id(self, obj):
+      return "plant-" + str(obj.id)
+
 class PlantSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.SerializerMethodField('label_id')
     leaves = serializers.RelatedField(many=True)
@@ -22,36 +37,106 @@ class PlantSerializer(serializers.HyperlinkedModelSerializer):
       return "plant-" + str(obj.id)
 
 class LeafSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField('label_id')
+    plant = serializers.RelatedField()
+
     class Meta:
         model = Leaf
         fields = ('id', 'plant', 'placement', 'blade', 'veins', 'location', 'date_found')
 
+    def label_id(self, obj):
+      return "leaf-" + str(obj.id)
+
+
 # ViewSets define the view behavior.
-class PlantViewSet(viewsets.ModelViewSet):
-    queryset = Plant.objects.all()
-    serializer_class = PlantSerializer
-    lookup_field = 'slug'
+class PlantList(APIView):
 
-class LeafViewSet(viewsets.ModelViewSet):
-    queryset = Leaf.objects.all()
-    serializer_class = LeafSerializer
+    def get(self, request, format=None):
+        plants = Plant.objects.all()
+        serializer = PlantListSerializer(plants, many=True)
+        return Response({'plants': serializer.data})
 
+    def post(self, request, format=None):
+        serializer = PlantSerializer(data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# Routers provide a way of automatically determining the URL conf.
-router = routers.SimpleRouter()
-router.register(r'dicots', PlantViewSet)
+class PlantDetail(APIView):
 
-dicots_router = routers.NestedSimpleRouter(router, r'dicots', lookup='dicots')
-dicots_router.register(r'leaf', LeafViewSet)
+    def get_object(self, slug):
+        try:
+            return Plant.objects.get(slug=slug)
+        except Plant.DoesNotExist:
+            raise Http404
 
+    def get(self, request, slug, format=None):
+        plant = self.get_object(slug)
+        serializer = PlantSerializer(plant)
+        return Response(serializer.data)
+
+    def put(self, request, slug, format=None):
+        serializer = PlantSerializer(data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, slug, format=None):
+      plant = self.get_object(slug)
+      serializer = PlantSerializer(plant, data=request.DATA, partial=True)
+      if serializer.is_valid():
+          serializer.save()
+          return Response(serializer.data)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, slug, format=None):
+        plant = self.get_object(slug)
+        plant.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class LeafDetail(APIView):
+
+    def get_plant_object(self, plant_slug):
+        try:
+            return Plant.objects.get(slug=plant_slug)
+        except Plant.DoesNotExist:
+            raise Http404
+
+    def get_object(self, plant_slug, pk):
+        plant = self.get_plant_object(plant_slug)
+        try:
+            return plant.leaves.get(pk=pk)
+        except Leaf.DoesNotExist:
+            raise Http404
+
+    def get(self, request, plant_slug, pk, format=None):
+        leaf = self.get_object(plant_slug, pk)
+        serializer = LeafSerializer(leaf)
+        return Response(serializer.data)
+
+    def post(self, request, plant_slug, pk, format=None):
+        leaf = self.get_object(plant_slug, pk)
+        serializer = LeafSerializer(leaf, data=request.DATA, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, plant_slug, pk, format=None):
+        leaf = self.get_object(plant_slug, pk)
+        leaf.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'plant_lyfe_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^', include(router.urls)),
-    url(r'^', include(dicots_router.urls)),
+    url(r'^dicots/$', PlantList.as_view()),
+    url(r'^dicots/(?P<slug>[\w-]+)/$', PlantDetail.as_view()),
+    url(r'^dicots/(?P<plant_slug>[\w-]+)/leaf/(?P<pk>\d+)/$', LeafDetail.as_view()),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
 )
